6854257d4dbe08143ec7c3f275909a822cdf8ff5
Ilan Gitter <8359193+gitteri@users.noreply.github.com>
2020-01-26 01:18:56 -0500

    fix for zeit

--- next.config.js
+++ next.config.js
@@ -1,34 +1,31 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-const repoNameURIPrefix =
-  process.env.NODE_ENV === 'production' ? '/cc-tools' : '';
+// TODO: Figure out how to deploy directly to github pages
+const repoNameURIPrefix = process.env.NODE_ENV === "production" ? "" : "";
 
 module.exports = {
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-      "/decode/hex": { page: "/decode/hex" },
-    }
-  },
-  generateBuildId: async () => 'current',
-  assetPrefix: repoNameURIPrefix,
-  env: {
-    linkPrefix: repoNameURIPrefix,
-  },
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  }/*,
+    exportPathMap: function() {
+        return {
+            "/": { page: "/" },
+            "/decode/hex": { page: "/decode/hex" }
+        };
+    },
+    generateBuildId: async () => "current",
+    assetPrefix: repoNameURIPrefix,
+    env: {
+        linkPrefix: repoNameURIPrefix
+    },
+    webpack: (config, { dev }) => {
+        // Perform customizations to webpack config
+        // console.log('webpack');
+        // console.log(config.module.rules, dev);
+        config.module.rules = config.module.rules.map(rule => {
+            if (rule.loader === "babel-loader") {
+                rule.options.cacheDirectory = false;
+            }
+            return rule;
+        });
+        // Important: return the modified config
+        return config;
+    } /*,
   webpackDevMiddleware: (config) => {
     // Perform customizations to webpack dev middleware config
     // console.log('webpackDevMiddleware');
@@ -36,4 +33,4 @@ module.exports = {
     // Important: return the modified config
     return config
   }, */
-}
+};
